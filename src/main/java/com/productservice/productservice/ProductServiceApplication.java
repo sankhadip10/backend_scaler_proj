8638820ec88af1acbc0b1fb289b994a5cf4bd6f3
Sankhadip Das <sankhadip10.das@gmail.com>
2023-12-04 23:57:12 +0530

    DB connectivity

--- src/main/java/com/productservice/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/productservice/ProductServiceApplication.java
@@ -1,13 +1,63 @@
 package com.productservice.productservice;
 
+//import com.productservice.productservice.inheritancerelations.mappedsuperclass.Mentor;
+import com.productservice.productservice.inheritancerelations.tableperclass.*;
+//import com.productservice.productservice.inheritancerelations.mappedsuperclass.MentorRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProductServiceApplication {
+import java.util.List;
 
+@SpringBootApplication
+public class ProductServiceApplication implements CommandLineRunner {
+    private MentorRepository mentorRepository;
+    private StudentRepository studentRepository;
+    private UserRepository userRepository;
+    ProductServiceApplication(@Qualifier("tpc_mentorrepository")
+                              MentorRepository mentorRepository,
+                              StudentRepository studentRepository,
+                              UserRepository userRepository){
+        this.mentorRepository = mentorRepository;
+        this.studentRepository = studentRepository;
+        this.userRepository = userRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+//        Mentor mentor = new Mentor();
+//        mentor.setName("Sankhadip");
+//        mentor.setEmail("sankhadip10.das@gmaiol.com");
+//        mentor.setAvgRating(4.8);
+//
+//        mentorRepository.save(mentor);
+
+        Mentor mentor = new Mentor();
+        mentor.setName("Ricky");
+        mentor.setEmail("ricky.das25@gmail.com");
+        mentor.setAvgRating(4.5);
+        mentorRepository.save(mentor);
+
+        Student student = new Student();
+        student.setName("Ram");
+        student.setEmail("ram.das25@gmail.com");
+        student.setPsp(80.0);
+        studentRepository.save(student);
+
+        User user = new User();
+        user.setEmail("archi@gmail.com");
+        user.setName("Archii");
+        userRepository.save(user);
+
+
+        List<User> users = userRepository.findAll();
+        for(User user1 :users){
+            System.out.println(user1.toString());
+        }
+
+    }
 }
---
+++ src/main/java/com/productservice/productservice/UUID.java
@@ -0,0 +1,8 @@
+package com.productservice.productservice;
+
+public class UUID {
+    public static void main(String[] args) {
+        java.util.UUID uuid = java.util.UUID.randomUUID();
+        System.out.println(uuid);
+    }
+}
