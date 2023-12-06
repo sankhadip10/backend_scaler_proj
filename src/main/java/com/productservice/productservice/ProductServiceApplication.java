b00afb6185d131b49537384669868e65f5f45700
Sankhadip Das <sankhadip10.das@gmail.com>
2023-12-06 07:14:08 +0530

    Representing inheritance relation in database

--- src/main/java/com/productservice/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/productservice/ProductServiceApplication.java
@@ -1,28 +1,38 @@
 package com.productservice.productservice;
 
 //import com.productservice.productservice.inheritancerelations.mappedsuperclass.Mentor;
-import com.productservice.productservice.inheritancerelations.tableperclass.*;
-//import com.productservice.productservice.inheritancerelations.mappedsuperclass.MentorRepository;
+
+import com.productservice.productservice.inheritancerelations.singletable.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class ProductServiceApplication implements CommandLineRunner {
     private MentorRepository mentorRepository;
-    private StudentRepository studentRepository;
     private UserRepository userRepository;
-    ProductServiceApplication(@Qualifier("tpc_mentorrepository")
-                              MentorRepository mentorRepository,
-                              StudentRepository studentRepository,
-                              UserRepository userRepository){
+    private StudentRepository studentRepository;
+
+    ProductServiceApplication(@Qualifier("st_mentorrepo") MentorRepository mentorRepository,
+                              @Qualifier("st_userrepo") UserRepository userRepository,
+                              @Qualifier("st_studentrepo") StudentRepository studentRepository){
         this.mentorRepository = mentorRepository;
-        this.studentRepository = studentRepository;
         this.userRepository = userRepository;
+        this.studentRepository = studentRepository;
     }
+
+//    private MentorRepository mentorRepository;
+//    private StudentRepository studentRepository;
+//    private UserRepository userRepository;
+//    ProductServiceApplication(@Qualifier("tpc_mentorrepository")
+//                              MentorRepository mentorRepository,
+//                              StudentRepository studentRepository,
+//                              UserRepository userRepository){
+//        this.mentorRepository = mentorRepository;
+//        this.studentRepository = studentRepository;
+//        this.userRepository = userRepository;
+//    }
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
@@ -35,29 +45,46 @@ public class ProductServiceApplication implements CommandLineRunner {
 //        mentor.setAvgRating(4.8);
 //
 //        mentorRepository.save(mentor);
+//
+//        Mentor mentor = new Mentor();
+//        mentor.setName("Ricky");
+//        mentor.setEmail("ricky.das25@gmail.com");
+//        mentor.setAvgRating(4.5);
+//        mentorRepository.save(mentor);
+//
+//        Student student = new Student();
+//        student.setName("Ram");
+//        student.setEmail("ram.das25@gmail.com");
+//        student.setPsp(80.0);
+//        studentRepository.save(student);
+//
+//        User user = new User();
+//        user.setEmail("archi@gmail.com");
+//        user.setName("Archii");
+//        userRepository.save(user);
+//
+//
+//        //Get all the Users
+//        List<User> users = userRepository.findAll();
+//        for(User user1 :users){
+//            System.out.println(user1.toString());
+//        }
+        User user =new User();
+        user.setName("Arshi");
+        user.setEmail("arshi@gmail.com");
+        userRepository.save(user);
 
         Mentor mentor = new Mentor();
-        mentor.setName("Ricky");
-        mentor.setEmail("ricky.das25@gmail.com");
-        mentor.setAvgRating(4.5);
+        mentor.setName("Deepak");
+        mentor.setEmail("sankhad@gmail.com");
+        mentor.setAvgRating(4.8);
         mentorRepository.save(mentor);
 
         Student student = new Student();
-        student.setName("Ram");
-        student.setEmail("ram.das25@gmail.com");
-        student.setPsp(80.0);
+        student.setName("Sankha");
+        student.setEmail("ricky.das25@gmail.com");
+        student.setPsp(99.0);
         studentRepository.save(student);
-
-        User user = new User();
-        user.setEmail("archi@gmail.com");
-        user.setName("Archii");
-        userRepository.save(user);
-
-
-        List<User> users = userRepository.findAll();
-        for(User user1 :users){
-            System.out.println(user1.toString());
-        }
 
 
     }
