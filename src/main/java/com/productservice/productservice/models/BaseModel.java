0946952c72bc334a4c84246f4fcca06cafc2abe7
Sankhadip Das <sankhadip10.das@gmail.com>
2023-12-12 21:49:43 +0530

    No major chanfes

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -21,20 +21,5 @@ public class BaseModel {
     private UUID id;
 
 }
-class Solution:
-        def findSpecialInteger(self, arr: List[int]) -> int:
-        if len(arr) < 2:
-        return arr[0]
-        result = 0
-        percent = int(len(arr) * 0.25)
-        count_map = {}
 
-        for i in arr:
-        count_map[i] = count_map.get(i, 0) + 1
-
-        for key, count in count_map.items():
-        if count > percent:
-        result = key
-
-        return result
 
