77c9ce2c81b81fb5d3b1d430828ee99bcbdc4f31
Sankhadip Das <sankhadip10.das@gmail.com>
2023-12-31 23:14:31 +0530

    Testing 2

--- src/test/java/com/productservice/productservice/controllers/ProductControllerTests.java
+++ src/test/java/com/productservice/productservice/controllers/ProductControllerTests.java
@@ -56,3 +56,14 @@ public class ProductControllerTests {
 }
 
 
+
+class Solution:
+        def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        ans = -1
+
+        for left in range(len(s)):
+        for right in range(left + 1, len(s)):
+        if s[left] == s[right]:
+        ans = max(ans, right - left - 1)
+
+        return ans
