f003279753625897148efa4a3732cebef0418e73
Sankhadip Das <sankhadip10.das@gmail.com>
2024-01-04 23:57:18 +0530

    Testing 2

--- src/test/java/com/productservice/productservice/controllers/ProductControllerTests.java
+++ src/test/java/com/productservice/productservice/controllers/ProductControllerTests.java
@@ -56,14 +56,16 @@ public class ProductControllerTests {
 }
 
 
-
 class Solution:
-        def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        ans = -1
-
-        for left in range(len(s)):
-        for right in range(left + 1, len(s)):
-        if s[left] == s[right]:
-        ans = max(ans, right - left - 1)
+        def minOperations(self, nums: List[int]) -> int:
+        mp=defaultdict(int)
+        for i in nums:
+        mp[i]+=1
+        ans=0
+        for i in mp.values():
+        if i==1:
+        return -1
+        ans+=(i+2)//3
 
         return ans
+
